20f8d3d6c1b8c6413d119851a894e98056f43985
qaiik <82181231+qaiik@users.noreply.github.com>
2025-03-17 21:14:27 -0600

    Update mcqanswer.js

--- mcqanswer.js
+++ mcqanswer.js
@@ -1,9 +1,13 @@
-let crid = LearnosityAssess.getCurrentItem().response_ids[0]
-let q = LearnosityAssess.getQuestions()[crid];
-let indices = q.validation.valid_response.value.map((n,i) => i);
-let answerUl = document.querySelector(`#${crid} > div.lrn_response_wrapper > div.lrn_response.lrn_clearfix > div:nth-child(3) > div.lrn-response-validate-wrapper > ul`)
-let answerChildren = Array.from(answerUl.children);
+function mcqAnswer(q, crid) {
+    // let crid = LearnosityAssess.getCurrentItem().response_ids[0]
+    // let q = LearnosityAssess.getQuestions()[crid];
+    let indices = q.validation.valid_response.value.map((n, i) => i);
+    let answerUl = document.querySelector(`#${crid} > div.lrn_response_wrapper > div.lrn_response.lrn_clearfix > div:nth-child(3) > div.lrn-response-validate-wrapper > ul`)
+    let answerChildren = Array.from(answerUl.children);
 
-for (let i = 0; i < indices.length; i++) {
-  answerChildren[i].firstChild.click()
+    for (let i = 0; i < indices.length; i++) {
+        answerChildren[i].firstChild.click()
+    }
+
+    LearnosityAssess.validateQuestions();
 }
