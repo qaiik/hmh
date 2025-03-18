b9db95f6cd936857e3a3a658c22b9505d3bb84fd
qaiik <82181231+qaiik@users.noreply.github.com>
2025-03-17 21:01:20 -0600

    Update bruteforce.js

--- bruteforce.js
+++ bruteforce.js
@@ -1,3 +1,19 @@
 function isCorrect(element) {
   return !element.ariaLabel.includes('incorrect')
 }
+
+function getChecks() {
+  return Array.from(document.querySelectorAll("button")).filter(n => n.className.includes("lrn_validate") && !n.className.includes("invisible") && n.className.includes("lds-btn-secondary"));
+}
+
+function getWrappers(checks) {
+  return checks.map(n => n.parentElement)
+}
+
+function getChildren(e) {
+  return Array.from(e.children)
+}
+
+function getAnswerElements(w) {
+  return w.map(w => getChildren(w).filter(c => c.ariaLabel?.includes("is equal to")))
+}
