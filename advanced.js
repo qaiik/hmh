740307f4e5bf252cdd020279d04ddd8e460c7c03
qaiik <82181231+qaiik@users.noreply.github.com>
2025-05-09 12:49:37 -0600

    Update advanced.js

--- advanced.js
+++ advanced.js
@@ -1,8 +1,6 @@
 !function(){
     //HACKLIB definitions
 
-    //Hacklib remove space using zero functions
-
     //hacklib_string
     function __hls(f) {
       if (typeof f !== "function") return null;
@@ -19,37 +17,32 @@
     }
 
   
-    function __hlCI(l, i) {
-      if (i >= 0) return i; else return l - (i < 0 ? -i : i)
-    }
-    //hacklib_slice
-    function __hlSlice(string, start, end) {
-      let ss = ""
-      for (let i = start; i < string.length; i++) {
-        if (i > end) {} else ss += string[__hlCI(string.length, i)];
-      }
-      return ss
-    }
-
+    //hacklib_teststringendswith
     function __hlTSE(s, t) {
-      let partition = "";
-      for (let i = s.length - t.length; i < s.length; i++) {
-         partition += i;
-      }
-      return s == partition;
+        if (t.length > s.length) return false;
+        
+        let partition = "";
+        for (let i = s.length - t.length; i < s.length; i++) {
+            partition += s[i];
+          }
+        return t == partition;
     }
 
     //Invalidate if RegExp.prototype.test was modified
-    if (__hlRS(__hls(RegExp.prototype.test)) !== 'functiontest(){[nativecode]}') return;
   
     function isNative(fn) {
-        return (/\{\s*\[native code\]\s*\}/).test(String(fn));
+        return __hlTSE(__hlRS(__hls(fn)),'{[nativecode]}')
     }
 
     function vj(json) {
+        if (!isNative(open)) return
+
         const w = window.open()
+        if (!isNative(w.document.write)) return;
+        if (!isNative(JSON.stringify)) return;
         w.document.write(`<pre>${JSON.stringify(json, null, 2)}</pre>`)
     }
+    if (!isNative(Array.prototype.map)) return
     vj(LearnosityAssess.getCurrentItem().questions.map(q=>{
         return {validation: q.validation, options: q.options}
     }))
